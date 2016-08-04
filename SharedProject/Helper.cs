97602f4e3bfdfc5f2f277514315ebb265f18beb0
Adnan Riaz <Adnan.Riaz@crownbet.com.au>
2016-08-04 12:06:51 +1000

    Initial commit

---
+++ SharedProject/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedProject
+{
+    public class Helper
+    {
+        public static string HelperMethod1()
+        {
+            Console.WriteLine("Helper1 method");
+
+            return "Helper1 method";
+        }
+    }
+}
